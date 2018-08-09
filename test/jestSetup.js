c7c330f59fcdcc72ab97a5c6baec134290442a75
Anton Nocum <anton.nocum@gmail.com>
2018-08-10 00:18:20 +0800

    Added some webpack aliases to Jest Setup

--- test/jestSetup.js
+++ test/jestSetup.js
@@ -1,9 +1,11 @@
 module.exports = {
-  verbose: true,
+  verbose: false,
   testEnvironment: 'node',
   moduleNameMapper: {
     '^client/(.*)': '<rootDir>/../client/$1',
+    '^components/(.*)': '<rootDir>/../client/components/$1',
     '^server/(.*)': '<rootDir>/../server/$1',
+    '^actions/(.*)': '<rootDir>/../client/actions/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/assetsStubber.js',
     '\\.(css)$': 'identity-obj-proxy',
   },
