e33f9df8eac471cc19ac7e71632524cfc5c24ea3
Anton Nocum <anton.nocum@gmail.com>
2018-08-10 11:34:15 +0800

    HomePage test - Added Snapshot testing and refactored code

--- test/client/containers/HomePage.test.js
+++ test/client/containers/HomePage.test.js
@@ -2,17 +2,31 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { HomePage } from 'client/containers/HomePage';
 import Button from 'client/components/Button/Button';
+import renderer from 'react-test-renderer';
 
 describe('HomePage container', () => {
-  test('button disabled when latitude and longitude not present', () => {
-    const props = {
-      condition: {},
-    };
+  const defaultProps = {
+    condition: {},
+    place: {},
+  };
 
+  const homepageComponent = props => (
+    <HomePage {...{ ...defaultProps, ...props }}/>
+  );
+
+  test('button disabled when latitude and longitude not present', () => {
     const wrapper = shallow(
-      <HomePage {...props}/>,
+      homepageComponent(),
     );
 
     expect(wrapper.find(Button).prop('disabled')).toBe(true);
+  });
+
+  test('renders correctly', () => {
+    const wrapper = renderer
+      .create(homepageComponent())
+      .toJSON();
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
