b0d3ceba944eb5735378f91f180a6ed05ff7750c
Anton Nocum <anton.nocum@gmail.com>
2018-08-13 21:31:11 +0800

    LabelCheckbox Component - Added red test

---
+++ test/client/components/LabelCheckbox.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LabelCheckbox from 'client/components/LabelCheckbox/LabelCheckbox';
+
+describe('LabelCheckbox Component', () => {
+  const defaultProps = {
+    label: 'test label',
+    name: 'test name',
+    value: 'test value',
+  };
+
+  it('is CHECKED if "checked props" is TRUE', () => {
+    const wrapper = shallow(<LabelCheckbox checked {...defaultProps}/>);
+    const checkboxes = wrapper.find('input');
+
+    expect(checkboxes.prop('checked')).toBe(true);
+  });
+});
