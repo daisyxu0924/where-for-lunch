33fb10cce9efbbbdcd1473d9986255674b30cf74
Anton Nocum <anton.nocum@gmail.com>
2018-08-14 02:05:08 +0800

    PriceOptions Component - Added test for comparing checked count

--- test/client/components/PriceOptions.test.js
+++ test/client/components/PriceOptions.test.js
@@ -7,10 +7,20 @@ describe('PriceOptions Component', () => {
     priceOptions: { selected: [] },
     action: jest.fn(),
   };
+
   it('renders 4 `LabelCheckbox`', () => {
     const wrapper = shallow(<PriceOptions { ...defaultProps }/>);
     const checkboxes = wrapper.find('LabelCheckbox');
 
     expect(checkboxes.length).toBe(4);
+  });
+
+  it('renders equal checked `LabelCheckbox` with selected props', () => {
+    // Valid selected values = 1,2,3,4
+    const props = { ...defaultProps, priceOptions: { selected: ['1', '2'] } };
+    const wrapper = shallow(<PriceOptions { ...props }/>);
+    const checkboxes = wrapper.find('[checked=true]');
+
+    expect(checkboxes.length).toBe(props.priceOptions.selected.length);
   });
 });
