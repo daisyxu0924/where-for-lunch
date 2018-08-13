1c0187baf1badda2c677da0757bdefaab1bb655a
Anton Nocum <anton.nocum@gmail.com>
2018-08-13 22:36:59 +0800

    Added constants to Jest's module mapper

--- test/jestSetup.js
+++ test/jestSetup.js
@@ -7,6 +7,7 @@ module.exports = {
     '^containers/(.*)': '<rootDir>/../client/containers/$1',
     '^components/(.*)': '<rootDir>/../client/components/$1',
     '^actions/(.*)': '<rootDir>/../client/actions/$1',
+    '^constants/(.*)': '<rootDir>/../client/constants/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/assetsStubber.js',
     '\\.(css)$': 'identity-obj-proxy',
   },
