7ced1b714f2edbd6041824fa35bf3b93dbcd3bb1
Anton Nocum <anton.nocum@gmail.com>
2018-08-14 00:34:30 +0800

    LabelCheckbox Component - Added onclick property

--- client/components/LabelCheckbox/LabelCheckbox.js
+++ client/components/LabelCheckbox/LabelCheckbox.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './LabelCheckbox.css';
 
-const LabelCheckbox = ({ checked, label, name, value }) => (
+const LabelCheckbox = ({ checked, label, name, onClick, value }) => (
   <label className={styles.root}>
     <input
       type="checkbox"
       name={name}
+      onClick={onClick}
       value={value}
       checked={checked}
     />
@@ -18,7 +19,11 @@ LabelCheckbox.propTypes = {
   checked: PropTypes.bool,
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
 };
 
 LabelCheckbox.defaultProps = {
