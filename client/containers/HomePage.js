e9efe09c6b95b471889932c0927374b263bdf11f
Anton Nocum <anton.nocum@gmail.com>
2018-08-16 04:17:47 +0800

    Added Details route and component

--- client/containers/HomePage.js
+++ client/containers/HomePage.js
@@ -1,21 +1,30 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Place from 'components/Place/Place';
+import PlaceLink from 'components/PlaceLink/PlaceLink';
 import Search from 'containers/Search';
+import Details from 'containers/Details';
 
 export class HomePage extends PureComponent {
   static propTypes = {
     place: PropTypes.object,
+    match: PropTypes.object,
   };
 
   render() {
     const { place } = this.props;
+    const PlaceComponent = place.id ?
+      <PlaceLink place={place} /> :
+      <Place place={place} />;
 
     return (
       <div className="homePageWrapper">
-        <Place place={place} />
-        <Search />
+        <Route path="/" exact component={ () => PlaceComponent } />
+        <Route path="/" exact component={ Search } />
+
+        <Route path="/details/:placeid" exact component={Details} />
       </div>
     );
   }
