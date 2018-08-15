5390a84527db0d3c081f5b7563e203cf03989701
Anton Nocum <anton.nocum@gmail.com>
2018-08-16 04:33:47 +0800

    Added test for PlaceDetails

--- test/client/components/PlaceDetails.test.js
+++ test/client/components/PlaceDetails.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import PlaceDetails from 'client/components/PlaceDetails/PlaceDetails';
 import renderer from 'react-test-renderer';
 
 describe('PlaceDetails Component', () => {
   const props = {
     place: {
-      rating: 2,
+      rating: 5,
     },
-    photos: [
-      'http://via.placeholder.com/350x150',
-    ],
   };
+
+  test('renders Stars Component', () => {
+    const wrapper = shallow(<PlaceDetails {...props} />);
+    expect(wrapper.find('Stars').length).toBe(1);
+  });
 
   test('renders properly', () => {
     const placeComponent = renderer
