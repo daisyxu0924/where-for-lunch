ff8cc4193d4c36234a46e291013473834b0e6492
Anton Nocum <anton.nocum@gmail.com>
2018-08-16 02:22:27 +0800

    Added fetch + set detail saga

--- client/actions/placeActionTypes.js
+++ client/actions/placeActionTypes.js
@@ -1,2 +1,4 @@
 export const FETCH_PLACES = 'fetchPlaces';
 export const SET_DETAILS = 'setDetails';
+export const FETCH_DETAILS = 'fetchDetails';
+export const SET_COMPLETE_DETAILS = 'setCompleteDetails';
--- client/reducers/placeReducer.js
+++ client/reducers/placeReducer.js
@@ -8,6 +8,9 @@ const placeReducer = handleActions(
     [actionTypes.SET_DETAILS](state, action) {
       return { ...state, ...action.payload };
     },
+    [actionTypes.SET_COMPLETE_DETAILS](state, action) {
+      return { ...state, details: action.payload };
+    },
   },
   initialState,
 );
