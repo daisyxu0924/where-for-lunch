205082b3611d945cea5e8650184e6323d31a6503
Jay Lord Torres <isens.jaylord@gmail.com>
2018-11-08 17:30:58 +0800

    test: Button

---
+++ test/client/components/Button.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Button from 'client/components/Button/Button';
+// import renderer from 'react-test-renderer';
+
+describe('Button', () => {
+  const theme = 'homepageClick';
+
+  test('must have disabled class when disabled is true', () => {
+    const dom = mount( <Button theme={theme} disabled = {true} /> )
+    const button = dom.childAt(0)
+    expect( button.hasClass( theme + 'Disabled' ) )
+    .toBe( true );
+  });
+
+  test('must dont have disabled class when disabled is false', () => {
+    const dom = mount( <Button theme={theme} disabled = {false} /> )
+    const button = dom.childAt(0)
+    expect( button.hasClass( theme + 'Disabled' ) )
+    .toBe( false );
+  });
+
+  test('must dont have disabled attribute when disabled is false', () => {
+    const dom = mount( <Button theme={theme} disabled = {false} /> )
+    const button = dom.childAt(0)
+    expect( button.getDOMNode().attributes.getNamedItem('disabled'))
+    .toBe(null);
+  });
+
+  test('must have disabled attribute when disabled is true', () => {
+    const dom = mount( <Button theme={theme} disabled = {true} /> )
+    const button = dom.childAt(0)
+    expect( button.getDOMNode().attributes.getNamedItem('disabled'))
+    .not.toBe(null);
+  });  
+
+
+});
+
+ 
