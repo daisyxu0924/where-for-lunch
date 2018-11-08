e55ebdcf00449b6bd59b2afa9823c78a0b912c84
Jay Lord Torres <isens.jaylord@gmail.com>
2018-11-08 16:47:19 +0800

    test: add utils.isEmpty

--- test/client/lib/utils.test.js
+++ test/client/lib/utils.test.js
@@ -1,5 +1,5 @@
 
-import { getRandom } from 'client/lib/utils';
+import { getRandom, isEmpty } from 'client/lib/utils';
 
 describe('getRandom', () => {
   test('returns null when passed in an empty list', () => {
@@ -7,5 +7,31 @@ describe('getRandom', () => {
   });
   test('returns null when passed in null', () => {
     expect(getRandom()).toBe(null);
+  });
+});
+
+describe('isEmpty', () => {
+  test('returns true when pass null', () => {
+    expect(isEmpty(null)).toBe(true);
+  });
+
+  test('returns true when pass undefined', () => {
+    expect(isEmpty(undefined)).toBe(true);
+  });
+
+  test('returns true when pass empty string', () => {
+    expect(isEmpty('')).toBe(true);
+  });
+
+  test('returns false when pass 0', () => {
+    expect(isEmpty(0)).toBe(false);
+  });
+
+  test('returns false when pass 0', () => {
+    expect(isEmpty(0)).toBe(false);
+  });
+  
+  test('returns false when pass double', () => {
+    expect(isEmpty(1.2)).toBe(false);
   });
 });
