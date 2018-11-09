d61c731b8bcd6f1d35bf7af5c89df1c1e63bfa63
Jay Lord Torres <isens.jaylord@gmail.com>
2018-11-10 03:01:07 +0800

    clean: businessPage

--- client/containers/BusinessPage.js
+++ client/containers/BusinessPage.js
@@ -13,22 +13,17 @@ import { newWindow } from '../lib/navigatorHelper';
 import GoogleMap from '../components/GoogleMap/GoogleMap';
 
 class BusinessPage extends Component {
-  state = {
-    showMap: false,
-  }
-  handleOnClickShowMap = () => {
-    this.setState({ showMap: true });
-  }
-  handleOnClickGotoLink = () => {
-    newWindow(this.props.place.url);
-  }
-  handleOnClickBack = () => {
-    this.props.history.goBack();
-  }
-  componentDidMount = () => {
-    this.props.fetchPlacedetails(this.props.place.id || this.props.match.params.id);
-    window.scrollTo(0, 200); // added this for u to see the changes that there add at the bottom
-  }
+  state = { showMap: false }
+
+  handleOnClickShowMap = () => this.setState({ showMap: true });
+
+  handleOnClickGotoLink = () => newWindow(this.props.place.url);
+
+  handleOnClickBack = () => this.props.history.goBack();
+
+  componentDidMount = () => this.props.fetchPlacedetails(this.props.place.id || this.props.match.params.id)
+    && window.scrollTo(0, 200); // added this for u to see the changes that there add at the bottom
+
   render() {
     const { place } = this.props;
     const gmapCoordinate = isEmpty(place.coordinates ? place.coordinates.lat : null);
@@ -36,11 +31,24 @@ class BusinessPage extends Component {
       <div className="homePageWrapper">
         <Place place={place} />
         <Link to={`detail/${place.id}`}>
-          <Button onClick={this.handleOnClickBack} theme="BusinessPage" icon={'back'} title='Back' />
+          <Button
+            icon={'back'}
+            onClick={this.handleOnClickBack}
+            theme="BusinessPage"
+            title='Back' />
         </Link>
-        <Button onClick={this.handleOnClickGotoLink} theme="BusinessPage" icon={'page'} title='Visit Website' />
-        <Button onClick={this.handleOnClickShowMap} theme="BusinessPage" icon={'map'} title='Show Map' disabled={gmapCoordinate} />
-        {this.state.showMap ? <GoogleMap coordinates={place.coordinates}/> : null }
+        <Button
+          icon={'page'}
+          onClick={this.handleOnClickGotoLink}
+          theme="BusinessPage"
+          title='Visit Website' />
+        <Button
+          icon={'map'}
+          onClick={this.handleOnClickShowMap}
+          theme="BusinessPage"
+          title='Show Map'
+          disabled={gmapCoordinate} />
+        { this.state.showMap ? <GoogleMap coordinates={place.coordinates}/> : null }
         <PlaceInfo place={place} />
         <Photo place={place} />
       </div>
@@ -59,10 +67,10 @@ const mapDispatchToProps = dispatch =>
   }, dispatch);
 
 BusinessPage.propTypes = {
+  fetchPlacedetails: PropTypes.func,
+  history: PropTypes.object,
   place: PropTypes.object,
   match: PropTypes.object,
-  history: PropTypes.object,
-  fetchPlacedetails: PropTypes.func,
 };
 
 export default connect(
