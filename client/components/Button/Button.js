2d89cddcdc09185fec47de1eff57d774c9499c78
Jay Lord Torres <isens.jaylord@gmail.com>
2018-11-09 19:43:51 +0800

    components: Button: add styles for diff types of button

--- client/components/Button/Button.js
+++ client/components/Button/Button.js
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './Button.css';
 
-const Button = ({ onClick, children, theme, disabled }) => {
+const Button = ({ onClick, children, theme, disabled, icon, ...attributes }) => {
   return (
     <button
       className={classNames({
         [styles.root]: true,
         [styles[theme]]: true,
+        [styles[icon]]: true,
       })}
       onClick={onClick}
       disabled={disabled}
+      {...attributes}
     >
       { children }
     </button>
@@ -24,6 +26,7 @@ Button.propTypes = {
   ]),
   onClick: PropTypes.func,
   theme: PropTypes.string,
+  icon: PropTypes.string,
   disabled: PropTypes.bool,
 };
 
