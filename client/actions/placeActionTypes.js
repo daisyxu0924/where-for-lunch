25efd55020925280a4004f238be63ca4d2bea6f4
Jay Lord Torres <isens.jaylord@gmail.com>
2018-11-11 18:55:12 +0800

    clean: placeActionTypes

--- client/actions/placeActionTypes.js
+++ client/actions/placeActionTypes.js
@@ -1,4 +1,3 @@
 export const FETCH_PLACES = 'fetchPlaces';
 export const SET_DETAILS = 'setDetails';
 export const FETCH_PLACEDETAILS = 'fetchPlacedetails';
-export const SET_PLACEDETAILS = 'fetchPlacedetails';
