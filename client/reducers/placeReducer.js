b81bfa715b61ca09d78aca9acfd3c496c4db64c4
Jay Lord Torres <isens.jaylord@gmail.com>
2018-11-12 01:17:00 +0800

    redux: add error message state

--- client/reducers/placeReducer.js
+++ client/reducers/placeReducer.js
@@ -4,11 +4,15 @@ import * as actionTypes from 'actions/placeActionTypes';
 const initialState = {
   // id: '8QYaPXIbIB2Ko1lh5kM1eQ',
   id: null,
+  error: '',
 };
 const placeReducer = handleActions(
   {
     [actionTypes.SET_DETAILS](state, action) {
-      return { ...state, ...action.payload };
+      return { ...state, error: null, ...action.payload };
+    },
+    [actionTypes.SET_ERROR](state, action) {
+      return { ...state, error: action.payload };
     },
   },
   initialState,
