51e8e203f15024dd54389f3ede258b1bde6e732c
Jay Lord Torres <isens.jaylord@gmail.com>
2018-11-13 02:20:31 +0800

    server: place: add error handling in /

--- server/routes/places.js
+++ server/routes/places.js
@@ -9,10 +9,13 @@ import { PageNotFound } from '../lib/resultHelper';
 
 const router = Router();
 router.get('/', async (req, res) => {
-  const list = await searchPlaces(toSearchPlacesParams(req.query));
-  if (list.length > 0) return res.send(list.map(i => fromSearchPlacesParams(i)));
-  PageNotFound(res);
-  // res.send(list.map(i => fromSearchPlacesParams(i)));
+  try {
+    const list = await searchPlaces(toSearchPlacesParams(req.query));
+    if (list.length > 0) return res.send(list.map(i => fromSearchPlacesParams(i)));
+    PageNotFound(res);
+  } catch(e) {
+    PageNotFound(res);
+  }
 });
 
 export default router;
