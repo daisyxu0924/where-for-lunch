90bb495929b37e60b251c3521de77bc35b23c0bc
Jay Lord Torres <isens.jaylord@gmail.com>
2018-11-13 02:04:50 +0800

    redux: place: add actions: setError, setLoading

--- client/reducers/placeReducer.js
+++ client/reducers/placeReducer.js
@@ -2,11 +2,20 @@ import { handleActions } from 'redux-actions';
 import * as actionTypes from 'actions/placeActionTypes';
 
 const initialState = {
+  id: null,
+  loading: false,
+  error: '',
 };
 const placeReducer = handleActions(
   {
     [actionTypes.SET_DETAILS](state, action) {
-      return { ...state, ...action.payload };
+      return { ...state, loading: false, error: null, ...action.payload };
+    },
+    [actionTypes.SET_ERROR](state, action) {
+      return { ...state, loading: false, error: action.payload };
+    },
+    [actionTypes.SET_LOADING](state, action) {
+      return { ...state, loading: action.payload };
     },
   },
   initialState,
